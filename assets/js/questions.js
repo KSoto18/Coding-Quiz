8715d0207b78ec961b4ad3af043938b23b50d470
KaptSprinkles <KSoto18@users.noreply.github.com>
2022-10-04 16:41:14 -0400

    added questions to js file

---
+++ assets/js/questions.js
@@ -0,0 +1,64 @@
+//Set of 10 basic coding questions:
+
+var questions = [
+    {
+        question: "What does HTML stand for?",
+        choices: ["Hypertext Markup Language", "Hypercode Marketing Language", "Hyper Markdown Language", "None of the above"],
+        correct: "Hypertext Markup Language",
+    },
+
+    {
+        question: "What is the value type of Boolean",
+        choices: ["True", "False", "True & False", "One"],
+        correct: "True & False",
+    },
+
+    {
+        question: "What is used for storing and transporting data?",
+        choices: ["Semantics", "Java", "CSS", "JSON"],
+        correct: "JSON",
+    },
+
+    {
+        question: "An Array is enclosed within ______.",
+        choices: ["Curly brackets { }", "Parentheses ( )", "Square brackets [ ]", "Quote marks ' '"],
+        correct: "Square brackets [ ]",
+    },
+
+    {
+        question: "Which method adds to the end of an Array",
+        choices: [".push()", ".sort()", ".unshift()", ".pop()"],
+        correct: ".push()",
+    },
+
+    {
+        question: "Which method adds to the end of an Array",
+        choices: [".push()", ".sort()", ".unshift()", ".pop()"],
+        correct: ".push()",
+    },
+
+    {
+        question: "Which is not a Logical Operator:",
+        choices: ["&&", "//", "||", "!"],
+        correct: "//",
+    },
+
+    {
+        question: "What does CSS stand for?",
+        choices: ["Colorful Style Sheets", "Cascading Style Sheets", "Computer Style Sheets", "Cool Style Sheets"],
+        correct: "Cascading Style Sheets",
+    },
+
+    {
+        question: "Which one of the following is a function expression?",
+        choices: ["var colors = function()", "function colors()", "Both are correct", "None of the above"],
+        correct: "var colors = function()",
+    },
+
+    {
+        question: "What method outputs a message to the web console?",
+        choices: ["const = ", "alert()", "function()", "console.log()"],
+        correct: "console.log()",
+    },
+
+]
